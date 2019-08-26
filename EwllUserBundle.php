6df122991c10b35fd2a4704cb49b7d334b92dda3
Maxim Salamakhin <343604@gmail.com>
2019-08-26 11:45:47 +0700

    init

---
+++ EwllUserBundle.php
@@ -0,0 +1,11 @@
+<?php namespace Ewll\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * {@inheritdoc}
+ */
+class EwllUserBundle extends Bundle
+{
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Ewll/UserBundle
